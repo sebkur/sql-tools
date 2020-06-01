bc96481bf8d87bdf10d61ad3faaffd8f36ce500e
Sebastian Kürten <sebastian@topobyte.de>
2020-06-01 10:32:36 +0200

    Add a MySQL dump and test parsing it

---
+++ core/src/test/java/de/mobanisto/sqltools/antlr/TestParseCreateDump.java
@@ -0,0 +1,54 @@
+package de.mobanisto.sqltools.antlr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BailErrorStrategy;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.mobanisto.antlr.CaseChangingCharStream;
+import de.mobanisto.antlr.mysql.MySqlLexer;
+import de.mobanisto.antlr.mysql.MySqlParser;
+import de.mobanisto.antlr.mysql.MySqlParser.ColumnCreateTableContext;
+import de.mobanisto.antlr.mysql.MySqlParser.RootContext;
+import de.mobanisto.sqltools.CreateTableFinder;
+
+public class TestParseCreateDump
+{
+
+	/**
+	 * Make sure that we can parse a complete SQL dump.
+	 */
+	@Test
+	public void testUppercase() throws IOException
+	{
+		InputStream input = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("mysql/dump.sql");
+
+		ANTLRInputStream antlrInput = new ANTLRInputStream(input);
+		CaseChangingCharStream antlrInputUpper = new CaseChangingCharStream(
+				antlrInput, true);
+		MySqlLexer lexer = new MySqlLexer(antlrInputUpper);
+		CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+		MySqlParser parser = new MySqlParser(tokenStream);
+		parser.setErrorHandler(new BailErrorStrategy());
+
+		RootContext root = parser.root();
+
+		CreateTableFinder finder = new CreateTableFinder();
+		new ParseTreeWalker().walk(finder, root);
+
+		Assert.assertEquals(3, finder.getList().size());
+
+		List<ColumnCreateTableContext> creates = finder.getList();
+		for (ColumnCreateTableContext create : creates) {
+			System.out.println(create.getText());
+		}
+	}
+
+}
